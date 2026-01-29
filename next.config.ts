c6c7f8a67700e94afa68b50144287879a799ff26
George (AI) <jeanpierrewebb@gmail.com>
2026-01-29 13:46:35 +0000

    Fix: Add remote image domains to next.config for Unsplash, Reddit, Imgur, Pinterest

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.reddit.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.redd.it',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.pinimg.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
